65b62c7c4c346e8be774d030c9c3891f651b2141
Jorge Fox D'Angelo <jgdfox@gmail.com>
2022-09-21 15:03:22 -0300

    cambia nombre

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
                 <Link>
                     <Image src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' alt='icono de la app' width={70} height={70} />
                     <Text color='white' h2>P</Text>
-                    <Text color='white' h3>okémon</Text>
+                    <Text color='white' h3>okémon!</Text>
                 </Link>
             </NextLink>
 
